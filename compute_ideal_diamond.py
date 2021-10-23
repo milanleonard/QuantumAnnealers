14327a23917d34d6588b8933285ecbd57645848e
Milan Leonard <milan.leoanrd@gmail.com>
2021-10-23 15:00:25 +1100

    adding costofdephasing

--- compute_ideal_diamond.py
+++ compute_ideal_diamond.py
@@ -21,26 +21,21 @@ def perform_sv_anneal(num_qubits : int, anneal_time : float, quboproblem : dict)
     ratio = np.real(annealer.expect[-1]) / annealer.get_optimal()
     return (ratio, annealer.get_optimal()) if 0 < ratio < 1 else (0, annealer.get_optimal())
 
-# %%
-NUM_QUBITS = 10
-PROBLEMS = [MaxCutNXProblem(NUM_QUBITS, j, (j*i*10000)) for j in np.arange(0.1,0.9,0.1) for i in range(1,15)]
+PROBLEMS = [MaxCutNXProblem(num_qubits, j, (j*i*10000)) for j in np.arange(0.1,0.95,0.05) for i in range(1,5) for num_qubits in range(5,11)]
 # %%
 def do_total_computation(problem : MaxCutNXProblem):
     print("j")
     qubo = problem.generate_qubo()
     output = problem.graph_features()
-    ratiosv, optimal = perform_sv_anneal(NUM_QUBITS, 3, qubo)
-    ratiodm = perform_dm_anneal(NUM_QUBITS, 3, qubo)
+    ratiosv, optimal = perform_sv_anneal(problem.num_qubits, 3, qubo)
+    ratiodm = perform_dm_anneal(problem.num_qubits, 3, qubo)
     output['algo_ideal'] = ratiosv
     output['algo_diamond'] = ratiodm
     output['optimal'] = optimal
     return output
 
-with multiprocessing.Pool(8) as pool:
+with multiprocessing.Pool(31) as pool:
     outputs = pool.map(do_total_computation, PROBLEMS)
-# outputs = []
-# for problem in PROBLEMS:
-#     outputs.append(do_total_computation(problem))
-# %%
-with open('all_data.pkl','wb') as f:
+
+with open('./data/costofdephasing.pkl','wb') as f:
     pickle.dump(outputs, f)
